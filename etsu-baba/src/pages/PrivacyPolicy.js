98a486f52419c30400bfc155a30bc7f0b90237ba
Salihu5906 <Suleimansalihu2007@gmail.com>
2025-08-06 21:41:06 +0100

    ies

--- etsu-baba/src/pages/PrivacyPolicy.js
+++ etsu-baba/src/pages/PrivacyPolicy.js
@@ -3,7 +3,7 @@ import React from "react";
 const PrivacyPolicy = () => {
   return (
     <div className="text-center py-20">
-      <h1 className="text-3xl font-bold">Privacy Policy</h1>
+      <h1 className="text-3xl font-bold">Privacy Policies</h1>
     </div>
   );
 };
